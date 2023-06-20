1b64910a85b0220ccfe9d6db7f1c4b70a033e095
shiva <shivanaik2398@gmail.com>
2023-06-20 21:29:38 +0530

    teams

--- routers/teams-router.js
+++ routers/teams-router.js
@@ -3,8 +3,10 @@ const teamsController = require('../controller/teams-controller');
 const store = require('../middleware/multer');
 
 const router = express.Router();
-const hostUpload = store.store.fields([{ name: 'bannerImg', maxCount: 1 }, { name: 'leadersProfile', maxCount: 500 },{ name: 'boardsProfile', maxCount: 500 }]);
+const hostUpload = store.store.fields([{ name: 'bannerImg', maxCount: 1 }]);
 
 exports.router = router.get('/getTeams',teamsController.getTeams)
                         .post('/updateTeams',hostUpload,teamsController.updateTeams)
+                        .post('/addUpdateTeamMember',store.store.single('profile'),teamsController.addUpdateTeamMember)
+                        .post('/deleteTeamMember',store.store.single('profile'),teamsController.deleteTeamMember)
                         .post('/saveTeams',hostUpload,teamsController.saveTeams);
